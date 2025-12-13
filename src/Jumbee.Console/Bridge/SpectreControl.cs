fbdff59214e24ffbe351ffbd9e753952bea78a4a
Allister Beharry <allister.beharry@gmail.com>
2025-12-12 23:57:57 -0400

    Use _updatesRequested to signal Render() required in control.

--- src/Jumbee.Console/Bridge/SpectreControl.cs
+++ src/Jumbee.Console/Bridge/SpectreControl.cs
@@ -1,6 +1,7 @@
 namespace Jumbee.Console;
 
 using System;
+using System.Threading;
 
 using ConsoleGUI.Common;
 using ConsoleGUI.Data;
@@ -24,92 +25,95 @@ public class SpectreControl<T> : Control, IDisposable where T : IRenderable
     #endregion
 
     #region Properties
-        public IRenderable Content 
+    public IRenderable Content 
+    {
+        get => _control;
+        set 
         {
-            get => _control;
-            set 
-            {
-                lock (UIUpdate.Lock)
-                {
-                    _control = value;
-                    Render();
-                }
-            }
+            _control = value;
+            Interlocked.Increment(ref _updatesRequested);
         }
-        #endregion
-    
-        #region Indexers
-        public override Cell this[Position position]
-        {
-            get
-            {
-                lock (UIUpdate.Lock)
-                {
-                    if (_bufferConsole.Buffer == null || position.X < 0 || position.X >= Size.Width || position.Y < 0 || position.Y >= Size.Height)
-                    {
-                        return _emptyCell;
-                    }
-                    else
-                    {
-                        return _bufferConsole.Buffer[position.X, position.Y];
-                    }
-                }
-            }
-        }
-        #endregion
-    
-        #region Methods
-        public void Dispose()
-        {
-            UIUpdate.Tick -= OnTick;
-        }
+    }
+    #endregion
 
-        private void OnTick(object? sender, UIUpdateTimerEventArgs e)
-        {
-            lock (e.Lock)
-            {
-                Render();
-            }
-        }
-
-        protected override void Initialize()
+    #region Indexers
+    public override Cell this[Position position]
+    {
+        get
         {
             lock (UIUpdate.Lock)
             {
-                // Resize the control to fill the available space
-                // We clip it to avoid issues if MaxSize is 'infinite' (though unlikely in this specific layout)
-                var targetSize = MaxSize;
-                if (targetSize.Width > 1000) targetSize = new ConsoleGuiSize(1000, targetSize.Height); 
-                if (targetSize.Height > 1000) targetSize = new ConsoleGuiSize(targetSize.Width, 1000);
-                
-                Resize(targetSize);
-    
-                // Resize buffer
-                _bufferConsole.Resize(Size);
-                
-                Render();
+                if (_bufferConsole.Buffer == null || position.X < 0 || position.X >= Size.Width || position.Y < 0 || position.Y >= Size.Height)
+                {
+                    return _emptyCell;
+                }
+                else
+                {
+                    return _bufferConsole.Buffer[position.X, position.Y];
+                }
             }
         }
-    
-        private void Render()
+    }
+    #endregion
+
+    #region Methods
+    public void Dispose()
+    {
+        UIUpdate.Tick -= OnTick;
+    }
+
+    private void OnTick(object? sender, UIUpdateTimerEventArgs e)
+    {
+        lock (e.Lock)
         {
-            if (Size.Width <= 0 || Size.Height <= 0)
+            if (_updatesRequested > 0)
             {
-                return;
+                Render();
+                Interlocked.Exchange(ref _updatesRequested, 0u); // Explicitly use 0u for uint
             }
-            
-            // Render Spectre content to buffer
-            _ansiConsole.Clear(true);
-            // We probably want to render with the full width of the control
-            // Spectre will look at the Profile.Width which comes from the IConsole.Size (BufferConsole.Size)
-            _ansiConsole.Write(_control);
-            Redraw();
         }
-        #endregion
+    }
+
+    protected override void Initialize()
+    {
+        lock (UIUpdate.Lock)
+        {
+            // Resize the control to fill the available space
+            // We clip it to avoid issues if MaxSize is 'infinite' (though unlikely in this specific layout)
+            var targetSize = MaxSize;
+            if (targetSize.Width > 1000) targetSize = new ConsoleGuiSize(1000, targetSize.Height); 
+            if (targetSize.Height > 1000) targetSize = new ConsoleGuiSize(targetSize.Width, 1000);
+            
+            Resize(targetSize);
+
+            // Resize buffer
+            _bufferConsole.Resize(Size);
+            
+            Render();
+        }
+    }
+
+    private void Render()
+    {
+        if (Size.Width <= 0 || Size.Height <= 0)
+        {
+            return;
+        }
+        
+        // Render Spectre content to buffer
+        _ansiConsole.Clear(true);
+        // We probably want to render with the full width of the control
+        // Spectre will look at the Profile.Width which comes from the IConsole.Size (BufferConsole.Size)
+        _ansiConsole.Write(_control);
+        Redraw();
+    }
+    #endregion
+
     #region Fields
     private readonly BufferConsole _bufferConsole;
     private readonly AnsiConsoleBuffer _ansiConsole;
     private IRenderable _control;
+    private uint _updatesRequested;
     private static readonly Cell _emptyCell = new Cell(Character.Empty);
     #endregion
 }
